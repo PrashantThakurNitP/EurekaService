c26498b5bcc050e1d81c9e3d633bb72bc8448b36
PrashantThakurNitP <prashant09.thakur@gmail.com>
2024-02-03 13:09:12 +0530

    Intialized repository

---
+++ src/test/java/publicis/sapient/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package publicis.sapient.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
